f93a17770c2da5714c3c38fe5a8f8a9789c9dbcc
Alexander Danyarkhodzhaev <alexdany657@gmail.com>
2024-01-04 16:28:09 +0300

    Added deviation estimation

--- count_one_round_avg.py
+++ count_one_round_avg.py
@@ -25,8 +25,16 @@ def main() -> int:
                 nums[j].append(0)
     legend = sorted(nums.keys())
     BUCK = 10
-    nums = [np.array(nums[_l]).reshape(BUCK, len(nums[_l]) // BUCK).mean(axis=0) for _l in legend]
-    plt.stackplot(np.arange(len(nums[0]))+1, *nums, labels=legend)
+    _nums = [np.array(nums[_l]).reshape(BUCK, len(nums[_l]) // BUCK) for _l in legend]
+    nums = [i.mean(axis=0) for i in _nums]
+    vars = [np.sqrt(i.var(axis=0)) for i in _nums]
+    x_ls = np.arange(len(nums[0]))+1
+    plt.stackplot(x_ls, *nums, labels=legend)
+    tmp = np.zeros_like(nums[0])
+    for n, v in zip(nums, vars):
+        tmp += n
+        plt.fill_between(x_ls, tmp - v, tmp + v, alpha=0.25, color="black")
+        # plt.plot(x_ls, tmp, color="black")
     plt.legend()
     plt.show()
     return 0
