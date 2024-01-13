91eb9a5ce0d6f70f7fe67bca47bcd853c34da3bc
Alexey Slizkov <elexunix@gmail.com>
2024-01-13 20:56:34 +0300

    Add strats1/elexunix.py, it is somewhat sassy

---
+++ strats1/elexunix.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# This is a clear and unencumbered strategy by @elexunix
+
+n_players, player_i, n_rounds = map(int, input().split())
+
+for i in range(n_rounds):
+  print(0)
+  input()
+
+# salt: 3498ugh398wrhnefv
